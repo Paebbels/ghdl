cfa6cf75d318fd73320d6fa6da0b9ccdb2f5df42
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-07-30 02:14:33 +0200

    Fixed handling of declarations with multiple identifiers, if this is the last item in the chain.

--- pyGHDL/dom/_Translate.py
+++ pyGHDL/dom/_Translate.py
@@ -496,6 +496,8 @@ def GetGenericsFromChainedNodes(
                     else:
                         generic = nextGeneric
                         break
+                else:
+                    generic = nodes.Null_Iir
             else:
                 generic = nodes.Get_Chain(generic)
 
@@ -549,6 +551,8 @@ def GetPortsFromChainedNodes(
                     else:
                         port = nextPort
                         break
+                else:
+                    port = nodes.Null_Iir
             else:
                 port = nodes.Get_Chain(port)
 
@@ -615,6 +619,8 @@ def GetParameterFromChainedNodes(
                 else:
                     parameter = nextParameter
                     break
+            else:
+                parameter = nodes.Null_Iir
         else:
             parameter = nodes.Get_Chain(parameter)
 
@@ -745,6 +751,8 @@ def GetDeclaredItemsFromChainedNodes(
                 else:
                     item = nextItem
                     break
+            else:
+                item = nodes.Null_Iir
         else:
             item = nodes.Get_Chain(item)
 
