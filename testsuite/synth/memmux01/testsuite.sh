dafe7cad55effc52072c8d6e0cfd0af2e40df0cb
Tristan Gingold <tgingold@free.fr>
2026-03-01 17:02:34 +0100

    testsuite/synth: add a test to memmux01

--- testsuite/synth/memmux01/testsuite.sh
+++ testsuite/synth/memmux01/testsuite.sh
@@ -4,7 +4,9 @@
 
 GHDL_STD_FLAGS=--std=08
 
-TESTS="memmux01 memmux02 memmux02b memmux03 memmux03b memmux04 memmux05 memmux07"
+TESTS="
+memmux01 memmux02 memmux02b memmux03 memmux03b memmux03c
+memmux04 memmux05 memmux07"
 
 for t in $TESTS; do
     GHDL_SYNTH_FLAGS=
